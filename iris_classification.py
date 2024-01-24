0057ac13e0f2ef9e8194104bd6654e3623774af5
Callet <elliot.callet@hotmail.com>
2024-01-24 21:30:19 +0100

    save changes

--- iris_classification.py
+++ iris_classification.py
@@ -56,6 +56,9 @@ def evaluation(model):
     plt.plot(N, train_score.mean(axis=1), label='train score')
     plt.plot(N, val_score.mean(axis=1), label='validation score')
     plt.legend()
+    
+    print('Train score :', model.score(X_train, y_train))
+    print('Test score :', model.score(X_test, y_test))
 #%%
 for name, model in model_dict.items():
     print(name)
@@ -67,9 +70,33 @@ from sklearn.pipeline import make_pipeline
 model = make_pipeline(StandardScaler(), SVC(random_state=5))
 
 evaluation(model)
+#%%
+from sklearn.model_selection import GridSearchCV
 
+param_grid = {
+    'svc__C': [0.1, 1, 10, 100],
+    'svc__gamma': [0.01, 0.1, 1, 10],
+    'svc__kernel': ['rbf']
+}
 
+grid = GridSearchCV(model, param_grid)
 
+grid.fit(X_train, y_train)
+evaluation(grid.best_estimator_)
+#%%
+model_2 = KNeighborsClassifier()
+param_grid = {'n_neighbors':range(1,40),
+              'metric':['euclidean','minkowski','manhattan']}
+grid = GridSearchCV(model_2, param_grid, cv=5)
+grid.fit(X_train, y_train)
+evaluation(grid.best_estimator_)
+#%%
+from sklearn.linear_model import SGDClassifier
+model_3 = make_pipeline(StandardScaler(), SGDClassifier(random_state=5))
+param_grid = {'sgdclassifier__loss':['hinge', 'log_loss', 'modified_huber', 'squared_hinge', 'perceptron', 'squared_error', 'huber', 'epsilon_insensitive', 'squared_epsilon_insensitive'],
+              'sgdclassifier__penalty':['l1','l2']}
+grid = GridSearchCV(model_3, param_grid)
+evaluation(grid)
 
 
 
